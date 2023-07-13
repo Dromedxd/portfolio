fcc98c3866e748d2f2f83b5a42500ac942f4e0c6
Dromedxd <129063795+Alexvdhcabeza@users.noreply.github.com>
2023-07-13 15:55:50 +0200

    added a new proyect

--- src/components/pageContent/projects.js
+++ src/components/pageContent/projects.js
@@ -4,6 +4,7 @@ import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import ReactPlayer from 'react-player';
 import Link from '@mui/material/Link';
+import { styled } from '@mui/system';
 
 import './../../index.css';
 import data from './pageContentText.json';
@@ -14,26 +15,41 @@ const djangoUrl =
 const javaGameUrl =
   'https://drive.google.com/uc?export=download&id=18pHYhMYvvBRQb6qabzEyglHxUUu91RtC';
 
+const codeIgniterURL =
+  'https://drive.google.com/uc?export=download&id=17iXIbtw4bkz3J-JoYAV54UcF4twU_xXk';
+
+const StyledContainer = styled(Box)({
+  display: 'flex',
+  justifyContent: 'center',
+  padding: 2,
+});
+
 function parseContent(jsonData) {
   const projectsData = jsonData.find((item) => item.IdContent === 'projects');
-  const { contenido, urlDjango, urlJava } = projectsData;
+  const { contenido, urlDjango, urlJava, urlCodeIgniter } = projectsData;
 
   return {
     contenido,
     urlDjango,
     urlJava,
+    urlCodeIgniter,
   };
 }
 
 function linkToVideo(url) {
   const videoURL = url;
-  return <ReactPlayer url={videoURL} controls style={{ maxWidth: '100%' }} />;
+  return (
+    <StyledContainer>
+      <ReactPlayer url={videoURL} controls style={{ maxWidth: '100%' }} />
+    </StyledContainer>
+  );
 }
 
 const parsedContent = parseContent(data);
 const contentSplit = parsedContent.contenido.split('/separator/');
 const djangoVideo = linkToVideo(djangoUrl);
 const javaGameVideo = linkToVideo(javaGameUrl);
+const codeIgniterVideo = linkToVideo(codeIgniterURL);
 
 function Projects() {
   return (
@@ -55,15 +71,22 @@ function Projects() {
         <Grid item xs={1} sm={1}></Grid>
         <Grid item xs={5} sm={5}>
           <Box p={2} sx={{ marginTop: 2 }} boxShadow={8}>
-            <Typography variant="h5">{contentSplit[1]}</Typography>
+            <Typography variant="h5" align={'center'}>
+              {contentSplit[1]}
+            </Typography>
+
             {
-              <Typography sx={{ marginTop: 2, marginBottom: 2 }}>
+              <Typography
+                align={'center'}
+                sx={{ marginTop: 2, marginBottom: 2 }}
+              >
                 {contentSplit[2]}
               </Typography>
             }
+
             {djangoVideo}
             <Link href={parsedContent.urlDjango} target="_blank" rel="noopener">
-              <Typography sx={{ marginTop: 2 }}>
+              <Typography sx={{ marginTop: 2 }} align={'center'}>
                 {parsedContent.urlDjango}
               </Typography>
             </Link>
@@ -71,16 +94,49 @@ function Projects() {
         </Grid>
         <Grid item xs={5} sm={5}>
           <Box p={2} sx={{ marginTop: 2 }} boxShadow={8}>
-            <Typography variant="h5">{contentSplit[3]}</Typography>
+            <Typography variant="h5" align={'center'}>
+              {contentSplit[3]}
+            </Typography>
             {
-              <Typography sx={{ marginTop: 2, marginBottom: 2 }}>
+              <Typography
+                sx={{ marginTop: 2, marginBottom: 2 }}
+                align={'center'}
+              >
                 {contentSplit[4]}
               </Typography>
             }
             {javaGameVideo}
             <Link href={parsedContent.urlJava} target="_blank" rel="noopener">
-              <Typography sx={{ marginTop: 2 }}>
+              <Typography sx={{ marginTop: 2 }} align={'center'}>
                 {parsedContent.urlJava}
+              </Typography>
+            </Link>
+          </Box>
+        </Grid>
+
+        <Grid item xs={3.5} sm={3.5}></Grid>
+
+        <Grid item xs={5} sm={5}>
+          <Box p={2} sx={{ marginTop: 2 }} boxShadow={8}>
+            <Typography variant="h5" align={'center'}>
+              {contentSplit[5]}
+            </Typography>
+            {
+              <Typography
+                sx={{ marginTop: 2, marginBottom: 2 }}
+                align={'center'}
+              >
+                {contentSplit[6]}
+              </Typography>
+            }
+            {codeIgniterVideo}
+            <Link
+              href={parsedContent.urlCodeIgniter}
+              target="_blank"
+              rel="noopener"
+            >
+              <Typography sx={{ marginTop: 2 }} align={'center'}>
+                {parsedContent.urlCodeIgniter}
               </Typography>
             </Link>
           </Box>
